e8365248c8ca5e7bce2a21f3457d6bc3695f21a2
Koreyoshiii <127462842+Koreyoshiii@users.noreply.github.com>
2023-08-17 09:25:44 +0800

    Update dy.py

--- dy.py
+++ dy.py
@@ -87,7 +87,10 @@ if st.button("Predict"):
       #b="Low risk"
   #st.success('The risk group:'+ b)
   
-  explainer = shap.KernelExplainer(xgb.predict,trainx)
+  
+  
+
+explainer = shap.KernelExplainer(xgb.predict,trainx)
   shap_values = explainer.shap_values(outputdf)
 
 
